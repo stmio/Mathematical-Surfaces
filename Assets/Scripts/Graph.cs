b1caeb118e1b602a2ab05930973f79f58664adeb
Sam Turner <98767222+stmio@users.noreply.github.com>
2022-02-27 18:24:10 +0000

    Add functions for three dimensional shapes

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -17,22 +17,12 @@ public class Graph : MonoBehaviour {
 
     void Awake() {
         float step = 2f / resolution;
-        Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step;
 
         points = new Transform[resolution * resolution];
 
-        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
-            if (x == resolution) {
-                x = 0;
-                z++;
-            }
-
+        for (int i = 0; i < points.Length; i++) {
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
-
-            point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
         }
@@ -40,15 +30,19 @@ public class Graph : MonoBehaviour {
 
     void Update() {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
-
         float time = Time.time;
-        for (int i = 0; i < points.Length; i++) {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
 
-            position.y = f(position.x, position.z, time);
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z++;
+                v = (z + 0.5f) * step - 1f;
+            }
 
-            point.localPosition = position;
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
