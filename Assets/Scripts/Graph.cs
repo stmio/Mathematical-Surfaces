f66a719cecf01c0cbb80758f18e1d065f4f32ba7
Sam Turner <98767222+stmio@users.noreply.github.com>
2022-02-27 15:28:42 +0000

    Add another dimension to the graph

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -20,11 +20,17 @@ public class Graph : MonoBehaviour {
         Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step;
 
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
 
-        for (int i = 0; i < points.Length; i++) {
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z++;
+            }
+
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
 
             point.localPosition = position;
             point.localScale = scale;
@@ -40,7 +46,7 @@ public class Graph : MonoBehaviour {
             Transform point = points[i];
             Vector3 position = point.localPosition;
 
-            position.y = f(position.x, time);
+            position.y = f(position.x, position.z, time);
 
             point.localPosition = position;
         }
