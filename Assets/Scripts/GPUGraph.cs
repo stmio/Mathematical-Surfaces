3dd4eb473d01ed213633286d00a509ea1c795f5a
Sam Turner <98767222+stmio@users.noreply.github.com>
2022-03-06 16:43:08 +0000

    Set positions of points rendered on GPU

--- Assets/Scripts/GPUGraph.cs
+++ Assets/Scripts/GPUGraph.cs
@@ -19,7 +19,7 @@ public class GPUGraph : MonoBehaviour {
     [SerializeField]
     Mesh mesh;
 
-    [SerializeField, Range(10, 200)]
+    [SerializeField, Range(10, 1000)]
     int resolution = 10;
 
     [SerializeField]
@@ -85,6 +85,8 @@ public class GPUGraph : MonoBehaviour {
         int groups = Mathf.CeilToInt(resolution / 8f);
         computeShader.Dispatch(0, groups, groups, 1);
 
+        material.SetBuffer(positionsId, positionsBuffer);
+        material.SetFloat(stepId, step);
         var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
         Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionsBuffer.count);
     }
